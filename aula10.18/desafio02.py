32ae513e7e3e2a78ba225476f578c7452bf06e0e
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-10-18 16:20:44 -0300

    desafio02.py

---
+++ aula10.18/desafio02.py
@@ -0,0 +1,8 @@
+idade = input("digite sua idade:")
+idade_do_parente = input("digite a idade de algum parente seu:")
+nome = input("informe seu nome:")
+multiplicacao = int(idade) * int(idade_do_parente)
+print(multiplicacao)
+print(nome)
+
+
