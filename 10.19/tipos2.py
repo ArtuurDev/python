d0cc62a464a590804d68b624c21be4eee1d8e250
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-10-19 16:10:18 -0300

    adicionado tipos2.py

--- 10.19/tipos2.py
+++ 10.19/tipos2.py
@@ -1,1 +1,14 @@
 # entender tipos e mudancas de tipo dentro do python
+
+a = int(50)
+b = "50"
+print(type(a))
+print(type(b))
+
+
+idade = int(input("qual sua idade? "))
+print(type(idade))
+
+# utilizacao do float: moedas, peso, temperatura, medidas e outros casos parecidos 
+peso = float(input("qual seu peso?"))
+print(type(peso))
