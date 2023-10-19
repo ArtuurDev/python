3bf681b16505fec2ef1718dbb55cc6b31577aefb
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-10-19 17:18:52 -0300

    adicionado  desafio02.py na aula10.19

---
+++ aula10.19/desafio02.py
@@ -0,0 +1,6 @@
+#Receba a altura de uma pessoa, crie um codigo que calcule o peso ideal.
+
+masculino_h = float(input("digite sua altura:"))
+peso_ideal = float(72.7 * masculino_h) - 58
+
+print("seu peso ideal é:", peso_ideal)
