f5d2c5de369e1f6fb8ca7832aae9af8507e67622
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-10-19 16:04:30 -0300

    adicionado F_strings.py

---
+++ 10.19/F_strings.py
@@ -0,0 +1,10 @@
+# Interpolando strings com codigo python
+nome = input("qual é o seu nome? ")
+
+# concatenação
+print("Seja muito bem vindo ", nome)
+
+
+
+#interpolaçãoo
+print(f'Seja muito bem vindo {nome}. Estavamos esperando por voce!!!')
