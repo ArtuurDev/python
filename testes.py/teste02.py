69984f9403ff649d8fa87c8998c731e9527939f9
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-10-24 12:34:37 -0300

    adicionado teste02.py

---
+++ testes.py/teste02.py
@@ -0,0 +1,7 @@
+# aprendendo interpolaçao
+# receba a altura de uma pessoa, crie um codigo que calcule o peso ideal 
+
+alt = float(input("informe sua altura:"))
+
+print(f"O peso ideal para o sexo masculino é,{(72.7 * alt) - 58} ")
+print(f"o peso ideal para o sexo femino é, {(62.1 * alt) - 44 }")
