7aa266081ce5368dfb2676ad7cc4c881d1d0e8d1
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-10-24 12:56:20 -0300

    adicionado teste03.py

---
+++ testes.py/teste03.py
@@ -0,0 +1,7 @@
+# aprendendo interpolçao
+# crie um codigo que receba um salario, que receba  o valor em porcentagem de reajuste e calcule o novo salario
+
+salario = float(input("informe seu salario:"))
+porcentagem = float(input("informe a porcentagem:"))
+                
+print(f'seu novo salario é {(salario * porcentagem / 100) + salario}')                
