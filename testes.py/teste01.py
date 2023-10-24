d7125f33103637192459e15e9b8bbca05c589719
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-10-24 12:20:16 -0300

    adicionado teste1.py

--- testes.py/teste01.py
+++ testes.py/teste01.py
@@ -1,1 +1,6 @@
+# aprendendo interpolçao
+# peca a temperatur em fahrenheit e trasforme em celsius
 
+fahrenheit = float(input("informe a temperatura em fahrenheit:"))
+
+print(f'A tempera em graus celcius é:, {5 * (fahrenheit - 32) / 9}')
