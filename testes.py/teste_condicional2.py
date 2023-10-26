3ebcc62ed746932e444a5c873ea963c7246817f4
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-10-26 12:54:34 -0300

    adicionado teste_condicional2.py

---
+++ testes.py/teste_condicional2.py
@@ -0,0 +1,16 @@
+var = input("digite uma letra qualquer:")
+
+if var == "a" or var == "A":
+        print(f"A letra {var} é uma vogal")
+elif var == "e" or var == "E":
+    print(f"A letra {var} é uma vogal")
+elif var == "i" or var == "I":
+      print(f"A letra {var} é uma vogal")
+elif var == "o" or var == "O":
+      print(f"A letra {var} é uma vogal")
+elif var == "u" or var == "U":
+      print(f"A letra {var} é uma vogal")
+else:
+      print(f"A letra {var} é uma consoante")
+
+print("fim algoritmo")
