1c863d8d219377aca2476e012478d2fe54e46eb9
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-11-03 04:06:19 -0300

    adicionado for.py

---
+++ aula 10.31/for.py
@@ -0,0 +1,21 @@
+#for i in range(5):
+#    print(i)
+
+#crianças = ("manu", "leo", "artur") 
+
+#for item in crianças:
+#    print(item)
+
+
+
+#exercicio
+#Faça um programa que receba a idade de pessoas, até que o valor -1 seja
+#informado. O programa deve calcular e mostrar a quantidade de pessoas
+#em cada faixa etária e a maior idade
+1 #Até 15 anos
+2 #De 16 a 40 anos
+3 #De 41 a 60 anos
+4 #Acima de 60 anos
+
+
+
