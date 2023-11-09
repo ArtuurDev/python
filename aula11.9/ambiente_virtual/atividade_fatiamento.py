3e9c428704a2177fab52460829d16999ef8d9bc2
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-11-09 15:46:51 -0300

    adicionadi gitignore do ambiente virtual aula11.9

---
+++ aula11.9/ambiente_virtual/atividade_fatiamento.py
@@ -0,0 +1,35 @@
+#faça o fatiamento de 10 palavras do texto abixo
+
+
+
+texto = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras."
+
+f1 = texto [0:5]
+print(f1)
+
+f2 = texto [5:9]
+print(f2)
+
+f3 = texto [9:16]
+print(f3)
+
+f4 = texto [16:19]
+print(f4)
+
+f5 = texto [19:31]
+print(f5)
+
+f6 = texto [31:34]
+print(f6)
+
+f7 = texto [34:45]
+print(f7)
+
+f8 = texto [45:49]
+print(f8)
+
+f9 = texto [49:54]
+print(f9)
+
+f10 = texto [54:61]
+print(f10)
