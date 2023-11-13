951083f25f2038945168f163b007be091e52fff7
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-11-12 22:56:25 -0300

    adicionado atividade2

---
+++ aula11.9/ambiente_virtual/atidade2_fatiamento.py
@@ -0,0 +1,37 @@
+#faça o fatiamento de 10 palavras do texto abixo
+
+
+
+texto = "Somos uma escola de tecnologia de informação que cria pontes entre pessoas, conhecimento e empresas. Ambiente que proporciona conexão, troca de conhecimentos e aprendizado compartilhado entre participantes, instrutores e empresas parceiras."
+
+f1 = texto [100:110]
+
+
+f2 = texto [110: 114]
+
+
+f3 = texto [114:126]
+
+
+
+f4 = texto [126:134]
+
+
+
+f5 = texto [134:140]
+
+
+
+f6 = texto [140:143]
+
+
+
+f7 = texto [143:157]
+
+
+
+#f8 = texto [157:49]
+
+
+print(f"{f1} {f2} {f3} {f4} {f5} {f6} {f7}")
+
