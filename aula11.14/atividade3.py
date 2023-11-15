5272ca600e3fb7dddd054b82923159ff00ae81ad
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-11-15 20:12:21 -0300

    adicionado atividade 3.py

---
+++ aula11.14/atividade3.py
@@ -0,0 +1,27 @@
+nota_1 = (float(input("digite nota 1:")))
+nota_2 = (float(input("digite nota 2:")))
+nota_3 = (float(input("digite nota 3:")))
+media_aluno_1 = (nota_1 + nota_2 + nota_3) / 3
+print("a media do primeiro aluno é",media_aluno_1)
+
+nota_4 = (float(input("digite a nota 1:")))
+nota_5 = (float(input("digite nota 2:")))
+nota_6 = (float(input("digite a nota 3:")))
+
+media_aluno_2 = (nota_4 + nota_5 + nota_6) / 3
+print("a media do segundo aluno é", media_aluno_2)
+
+nota_7= (float(input("digite a nota 1:")))
+nota_8 = (float(input("digite nota 2:")))
+nota_9 = (float(input("digite a nota 3:")))
+media_aluno_3 = (nota_7 + nota_8 + nota_9) / 3
+print("a media do terceiro aluno é", media_aluno_3)
+
+nota_10= (float(input("digite a nota 1:")))
+nota_11= (float(input("digite nota 2:")))
+nota_12= (float(input("digite a nota 3:")))
+media_aluno_4 = (nota_10 + nota_11 + nota_12) / 3
+print("a media do quarto aluno é", media_aluno_4)
+
+print("fim")
+
