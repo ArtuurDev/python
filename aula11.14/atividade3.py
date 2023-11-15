f210e11174dcb29cebe6a64b44242084c50e2197
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-11-15 20:52:56 -0300

    atividade3.py

--- aula11.14/atividade3.py
+++ aula11.14/atividade3.py
@@ -23,5 +23,7 @@ nota_12= (float(input("digite a nota 3:")))
 media_aluno_4 = (nota_10 + nota_11 + nota_12) / 3
 print("a media do quarto aluno é", media_aluno_4)
 
+lista_media_alunos = [media_aluno_1,media_aluno_2,media_aluno_3,media_aluno_4]
+print("A lista das medias é",lista_media_alunos)
 print("fim")
 
