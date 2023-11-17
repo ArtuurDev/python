cb4bd1d2327147a30ed1b7d11c31f2e2c7223744
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-11-17 15:43:11 -0300

    aticvidade3

---
+++ aula11.16/atividade_listas3.py
@@ -0,0 +1,14 @@
+# com meus_nomes = ['Ricardo', 'Roberto', 'Soares', 'Rivaldo', 'Richard', 'Andre']
+# imprima na tela uma lista só com os nomes que não começam com 'R'
+# remova os nomes dos indices impares
+
+
+meus_nomes = ['Ricardo', 'Roberto', 'Soares', 'Rivaldo', 'Richard', 'Andre']
+
+l_r = meus_nomes[2],meus_nomes[5]
+print(l_r)
+
+meus_nomes.remove("Ricardo")
+meus_nomes.remove('Soares')
+meus_nomes.remove('Richard')
+print(meus_nomes)
