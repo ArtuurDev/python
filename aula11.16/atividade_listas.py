bc1b88a79105882192a71089db6abc2cd4ca53fa
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-11-17 00:02:18 -0300

    atualizado

--- aula11.16/atividade_listas.py
+++ aula11.16/atividade_listas.py
@@ -2,6 +2,7 @@
 # remova da lista os multiplos de 4
 
 meus_numeros = [0, 1, 2, 3, 4, 5, 6,7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+pares_ok = []
 pares_ok = meus_numeros[0::2]
 print(pares_ok)
 pares_ok.remove(0)
