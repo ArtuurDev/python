ca4549f5e006ba20d495f1ced5c22155083b1a72
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-11-20 19:06:58 -0300

    adicionado exercicio5

---
+++ atividades_laços e loops/exercicos5.py
@@ -0,0 +1,11 @@
+#Faça um programa que peça dois números, base e expoente, 
+#calcule e mostre o primeiro 
+#número elevado ao segundo número. 
+
+ 
+# fazendo sem usar for nem while 
+
+base = int(input("digite a base:"))
+expoente = int(input("digite o expoente:"))
+calculo = base ** expoente
+print(calculo)
