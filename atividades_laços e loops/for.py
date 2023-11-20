13d7628ccb687ae3cf66b318ca1d414cab974c1a
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-11-20 13:50:54 -0300

    adicionado for

---
+++ atividades_laços e loops/for.py
@@ -0,0 +1,10 @@
+#Faça um programa, utilizando for, que permita o usuário fazer três contas de subtração.
+
+
+for i in range(3):
+    number1 = int(input("digite um numero: "))
+    number2 = int(input("digite outro numero: "))
+    calculo = number1 - number2
+    print(f'o resultado da subtraçao é: {calculo}')
+    
+    
