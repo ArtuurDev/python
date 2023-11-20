49e455acb045549958867c00e54f826966d1406e
Artuur10 <arturcastrodossantos.com@gmail.com>
2023-11-20 16:33:41 -0300

    adicionado for

---
+++ aula11.20/atv.while3.py
@@ -0,0 +1,9 @@
+contador = 0
+while contador <= 5:
+    num1 = int(input("digite o primeiro numero:"))
+    num2 = int(input("digite o segundo numero numero:"))
+    num3 = int(input("digite o terceiro numero numero:"))
+    num4 = int(input("digite o quarto numero numero:"))
+    num5 = int(input("digite o quinto numero numero:"))
+    break
+contador +=1
