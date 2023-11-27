1f461e5e9b1ba7e4de35e1af90b4aa82c2e35e14
artuur <arturcastrodossantos.com@gmail.com>
2023-11-27 12:43:44 -0300

    atualizado

--- testes.py/teste04.py
+++ testes.py/teste04.py
@@ -1,3 +1,8 @@
+
+import math
+
+
+
 cateto_adjacente = float(input("informe o valor do cateto adjacente:"))
 cateto_oposto = float(input("informe o valor do cateto oposto:"))
 
@@ -5,9 +10,9 @@ print("o valor informado do cateto_adjacente é:", cateto_adjacente)
 print("o valor do cateto oposto é:",cateto_oposto)
 
 calculo_catetos = (cateto_adjacente **2) + (cateto_oposto **2)
+print(calculo_catetos)
 
 
+raiz =  math.sqrt(calculo_catetos)
 
-hipotenesua = calculo_catetos **(1/2)
-
-print(" o valor da hipotenusa é:", hipotenesua)
+print(" o valor da hipotenusa é:", raiz)
