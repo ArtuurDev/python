7f31748c0452cfbd948f0faa2e3488f1bdff3f88
artuur <arturcastrodossantos.com@gmail.com>
2023-11-27 01:21:04 -0300

    atividade 1 do guanabara concluido

--- atividades/atv1.py
+++ atividades/atv1.py
@@ -1,3 +1,6 @@
 #faça um programa que peça um numero e mostre seu antessesor e sucessor
 
 n = int(input("digite um numero: "))
+a = n -1
+s = n +1
+print("o numero trabalhado é {} seu antessesor é {} seu sucessor é {}".format(n,a,s))
