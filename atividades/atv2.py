162c7de2b7db12459407ec09887c0fc97d5b1d3c
artuur <arturcastrodossantos.com@gmail.com>
2023-11-27 02:15:08 -0300

    adicionado atv2 do curso do guanabara

---
+++ atividades/atv2.py
@@ -0,0 +1,4 @@
+#mostre o dobro, triplo, raiz quadrada de um numero
+
+n = int(input("digite um numero: "))
+print("o dobro do numero é {},\n o triplo é {},\n a raiz quadrada é {:.2f}".format(n*2,n*3,n**(1/2))) 
