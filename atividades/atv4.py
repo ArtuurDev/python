7bc3c5c893b8cb1b9217f9512ac9a80b526bb663
artuur <arturcastrodossantos.com@gmail.com>
2023-11-27 10:11:40 -0300

    atividae4 concluida

---
+++ atividades/atv4.py
@@ -0,0 +1,5 @@
+#faça um progra que peça uma distancua em metros e converta em metros e centimetros  e milimetros
+
+
+m = float(input("digite a distancia em metros : "))
+print(" a distancia em metros é {}, convertido em centimetros é {}, e em milimetros é {}".format(m, m * 100, m * 1000))
