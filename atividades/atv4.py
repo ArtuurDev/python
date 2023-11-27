d24ed83a9bf68ca2d8371e066961235617dc6325
artuur <arturcastrodossantos.com@gmail.com>
2023-11-27 10:22:21 -0300

    atidade4 atualizada

--- atividades/atv4.py
+++ atividades/atv4.py
@@ -1,5 +1,16 @@
 #faça um progra que peça uma distancua em metros e converta em metros e centimetros  e milimetros
 
-
+''''
 m = float(input("digite a distancia em metros : "))
 print(" a distancia em metros é {}, convertido em centimetros é {}, e em milimetros é {}".format(m, m * 100, m * 1000))
+'''
+#faça um programa que peça distancua em km e trasforme em metros e centimetros
+''''
+km = float(input("digite a distancia em km: "))
+print("a distamcia em km conervertida em metros é {}, e convertida em centimetros é {}".format(km * 100, km * 1000))
+'''
+
+#trasforme metros em km
+
+m = float(input("digite a distancia em metros: "))
+print("a distancia em metros é {}, e em kilometros é {} ".format(m, m / 1000))
