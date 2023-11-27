9a3c717a7f334ab3ef803121ebf13c1287872461
artuur <arturcastrodossantos.com@gmail.com>
2023-11-27 13:07:59 -0300

    adicionado atv1 de modulos

---
+++ MODULOS/atv1.py
@@ -0,0 +1,8 @@
+#faça uma programa que leia um numero real e mostre sua porçao intera
+#funçao floor retona uma valor arredondado para baixo
+import math
+
+
+n = float(input("digite um numero real:  "))
+
+print("o numero em sua porçao inteira é {}".format(math.floor(n)))
