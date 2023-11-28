128e84948075724f943726ec87f83dd13cfea1c1
artuur <arturcastrodossantos.com@gmail.com>
2023-11-28 08:01:11 -0300

    atualizado

--- aula11.23/count.py
+++ aula11.23/count.py
@@ -1,4 +1,4 @@
-# count - contar quantas vezes um determinado caractere aparece eum uma string
+# count - contar quantas vezes um determinado caractere aparece em uma string
 
 string = "a banana é amaraela e o abacate é verde."
 letra = "a" 
