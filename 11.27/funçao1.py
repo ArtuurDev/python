90063b6e793ac9ea5cd786b96769153410356385
artuur <arturcastrodossantos.com@gmail.com>
2023-11-28 15:21:58 -0300

    adicionado funçao

---
+++ 11.27/funçao1.py
@@ -0,0 +1,8 @@
+
+
+def soma(n1,n):
+    print("o resultado entre {} + {} é = {}".format(n,n1,n+n1))
+
+
+def sub(n,n1):
+    print("o resultado entre {} - {} é = {}".format(n,n1,n-n1))
---
+++ 11.27/funçao2.py
@@ -0,0 +1,9 @@
+import funçao1
+a = int(input("digite um numero"))
+b= int(input("digite um numero"))
+
+funçao1.soma(a,b)
+
+
+
+funçao1.sub(a,b)
