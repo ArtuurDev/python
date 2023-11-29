f6fcbb2e4ed957ec2b7ec08ae516eac94dc58023
artuur <arturcastrodossantos.com@gmail.com>
2023-11-29 20:41:21 -0300

    adicionado atv6

---
+++ MODULOS/atv6.py
@@ -0,0 +1,7 @@
+# faça uma programa que leia se o usiario tem silva no nome
+
+
+
+nome  = str(input("digite seu nome : ")).strip()
+print("seu nome tem silva? {}".format("silva" in nome.lower()))
+
