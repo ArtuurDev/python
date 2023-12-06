cbcdd45d9f1149cf55308e67b1dd83817f19516c
artuur <arturcastrodossantos.com@gmail.com>
2023-12-06 14:56:02 -0300

    adicionado atv.py

---
+++ aula12.06/atv.py
@@ -0,0 +1,11 @@
+# 1. crie uma lista de alunos cm o nome e nota final de cada aluno e coloque e um dicionario, depois imprima
+
+
+lista = [['artur',10], ['elvis' ,10], ['cedex', 10]]
+print(lista)
+dic = {}
+
+dic.update(lista)
+print(dic)
+
+print(lista[0])
