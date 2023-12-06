914f6bf08c0f68e7aaacb4cc84686d7da1ace32c
artuur <arturcastrodossantos.com@gmail.com>
2023-12-06 14:38:14 -0300

    adicionado dic

--- aula12.06/dic.py
+++ aula12.06/dic.py
@@ -2,6 +2,22 @@
 
 
 dic = {"nome": 'paulo'} # created
+dic2 = dict(idade = 23)
 
-print(dic) # readed
+dic['nome'] #readed
+reading = dic2.get('idade') #readed
 
+dic.update(sobrenome = 'junior') # updated
+
+
+
+dic.update({ 'idade': 23})
+
+
+print(dic)
+print(dic2)
+
+
+dic.clear() #deleted
+
+print('dados do dicionario:', dic)
