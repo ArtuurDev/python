90b29b38b27b1a809e759fe286bf1771c623947d
artuur <arturcastrodossantos.com@gmail.com>
2023-12-11 00:47:25 -0300

    atualizado listas

---
+++ LISTAS/listas2.py
@@ -0,0 +1,10 @@
+dados= ["pedro", 25,
+        "maria", 19,
+        "artur", 17]
+
+pessoas = []
+pessoas.append(dados[::])
+print(pessoas)
+
+pessoas = [["pedro",25],["artur",17]]
+print(pessoas[0])
